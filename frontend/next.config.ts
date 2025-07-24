391e7507ca6cdbd8e921530a4318619e06a87fc3
Stefan Kandlbnder <stefan.kandlbinder@gmail.com>
2025-07-24 23:57:16 +0200

    Update Next.js configuration to enhance image handling and introduce experimental server actions
    - Changed image domain configuration to use remote patterns for improved flexibility.
    - Added experimental server actions with a body size limit of 2mb to support larger payloads.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,14 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["gray-abstract-hyena-886.mypinata.cloud"],
+    remotePatterns: [
+      new URL("https://gray-abstract-hyena-886.mypinata.cloud/**"),
+    ],
   },
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
 };
 
 export default nextConfig;
